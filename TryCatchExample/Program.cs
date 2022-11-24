5356fddcc9c611cf0b8760dfb7781c279dcf584b
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-24 11:10:48 +0530

    Добавьте файлы проекта.

---
+++ TryCatchExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TryCatchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                int shelves = Convert.ToInt32(Console.ReadLine());
+                var result = drinks / shelves;
+                Console.WriteLine(result);
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("At least 1 shelf");
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please insert an integer");
+            }
+        }
+    }
+}
